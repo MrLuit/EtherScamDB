6ec03820220114a7da17a51c6697f543baf51955
MrLuit <me@luithollander.nl>
2017-08-03 21:30:15 +0200

    Fixed illegal return

--- _static/js/reportaddress.js
+++ _static/js/reportaddress.js
@@ -1,49 +1,45 @@
-var reportType;
 var args = {};
 
 function finish() {
-	$(".captcha").fadeOut('',function() {
-		$(".loading").fadeIn('', function() {
-			$.post("https://lu1t.nl/report.php", {
-				reportType: reportType,
-				args: args
-			}).done(function(data) {
-				$(".loading").fadeOut('', function() {
-					$(".end").fadeIn();
-				});
-			});
-		});
-	});
+    $(".captcha").fadeOut('', function() {
+        $(".loading").fadeIn('', function() {
+            $.post("https://lu1t.nl/report.php", {
+                reportType: 'generalAddressReport',
+                args: args
+            }).done(function(data) {
+                $(".loading").fadeOut('', function() {
+                    $(".end").fadeIn();
+                });
+            });
+        });
+    });
 }
 
 function reCaptchaVerify(response) {
-  if (response === document.querySelector('.g-recaptcha-response').value) {
-	  args['captcha'] = response;
-    finish();
-  }
+    if (response === document.querySelector('.g-recaptcha-response').value) {
+        args['captcha'] = response;
+        finish();
+    }
 }
 
 function reCaptchaExpired() {
-  /* do something when it expires */
+    /* do something when it expires */
 }
 
 function reCaptchaCallback() {
-  grecaptcha.render('g-recaptcha', {
-    'sitekey': '6LfTSysUAAAAAOIYE_x9aZuqBNRlzTRbHlMRpAiK',
-    'callback': reCaptchaVerify,
-    'expired-callback': reCaptchaExpired
-  });
+    grecaptcha.render('g-recaptcha', {
+        'sitekey': '6LfTSysUAAAAAOIYE_x9aZuqBNRlzTRbHlMRpAiK',
+        'callback': reCaptchaVerify,
+        'expired-callback': reCaptchaExpired
+    });
 }
 
 window.addEventListener("load", function() {
-       var results = new RegExp('[\?&]([^&#]*)').exec(window.location.href);
-        if (results == null) {
-            return null;
-        } else {
-            $("#address").val(decodeURIComponent(results[1]).toString() || 0);
-        }
+    var results = new RegExp('[\?&]([^&#]*)').exec(window.location.href);
+    if (results != null) {
+        $("#address").val(decodeURIComponent(results[1]).toString() || 0);
+    }
     $("#9sendb").click(function() {
-        reportType = 'generalAddressReport';
         args['address'] = $("#address").val();
         $(".question9b").fadeOut('', function() {
             $(".question10").fadeIn();
@@ -53,7 +49,7 @@ window.addEventListener("load", function() {
     $("#10send").click(function() {
         args['reason'] = $("#reason").val();
         $(".question10").fadeOut('', function() {
-			$(".captcha").fadeIn();
+            $(".captcha").fadeIn();
         });
     });
 
