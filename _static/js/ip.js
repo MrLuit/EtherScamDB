bbd56345f5d017a0cca445ffa66057dca9ba73a2
Luit Hollander <MrLuit@users.noreply.github.com>
2017-12-24 00:11:50 +0100

    Update data files

--- _static/js/ip.js
+++ _static/js/ip.js
@@ -4,4 +4,9 @@ window.addEventListener("load", function() {
         $("#location").html('<span>' + flag + data.country_name + '</span>');
         $("#map").html('<iframe width="100%" height="400px" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBw2MrKbJrjg6NYIq6kClIVvUx3vSj-a1s&q=' + data.latitude + ',' + data.longitude + '" allowfullscreen></iframe>');
     });
+	setTimeout(function() {
+		if($("#location").html() == "loading...") {
+			$("#location").html("loading... <b>(This seems to be taking a long time, adblock might be blocking the HTTP request)</b>");
+		}
+	},2000);
 });
