3a5f041c3c68afc75674c1f36ef9de40f5b334a9
Luit Hollander <MrLuit@users.noreply.github.com>
2018-02-10 21:23:15 +0100

    Accidentaly pasted some stuff from js beautifier in

--- run.js
+++ run.js
@@ -700,10 +700,4 @@ if (!fs.existsSync('config.js')) {
     getCache(function() {
         startWebServer();
     });
-} // This is just a sample script. Paste your real code (javascript or HTML) here.
-
-if ('this_is' == /an_example/) {
-    of_beautifier();
-} else {
-    var a = b ? (c % d) : e[f];
 }
