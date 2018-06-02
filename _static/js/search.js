d93db2b37af5fa014fafcde8aecc69e06c4e5588
hahnmichaelf <hahn.michael.f@gmail.com>
2018-06-02 14:07:45 -0400

    remove protocol www and trailing slashes

--- _static/js/search.js
+++ _static/js/search.js
@@ -8,15 +8,15 @@ function hideEverything() {
 
 window.addEventListener("load", function() {
     $('.ui.button').click(function() {
-        $.getJSON("/api/check/" + encodeURIComponent($("input").val().toLowerCase()), function(result) {
+        $.getJSON("/api/check/" + encodeURIComponent($("input").val().toLowerCase().replace('http://','').replace('https://','').replace('www.','').split(/[/?#]/)[0]), function(result) {
             if (result.result == 'verified') {
                 hideEverything();
-                $("#verifiedmessage").html(encodeURI($("input").val().toLowerCase()) + ' is a verified domain. You can trust the contents.');
+                $("#verifiedmessage").html(encodeURI($("input").val().toLowerCase().replace('http://','').replace('https://','').replace('www.','').split(/[/?#]/)[0]) + ' is a verified domain. You can trust the contents.');
                 $("#verifiedmessage").html($("#verifiedmessage").html());
                 $("#verified").css('display', 'flex');
             } else if (result.result == 'neutral') {
                 hideEverything();
-                $("#neutralmessage").html(encodeURI($("input").val().toLowerCase()) + ' wasn\'t recognized as a malicious domain, nor as verified domain. Be careful!');
+                $("#neutralmessage").html(encodeURI($("input").val().toLowerCase().replace('http://','').replace('https://','').replace('www.','').split(/[/?#]/)[0]) + ' wasn\'t recognized as a malicious domain, nor as verified domain. Be careful!');
                 $("#neutralmessage").html($("#neutralmessage").html());
                 $("#neutral").css('display', 'flex');
             } else if (result.result == 'blocked') {
@@ -24,13 +24,13 @@ window.addEventListener("load", function() {
                 blocked = true;
                 var strLink = '';
                 if (result.type == 'domain' && 'category' in result.entries[0]) {
-                    $("#blacklistmessage").html(encodeURI($("input").val().toLowerCase()) + ' was put on the blacklist for ' + result.entries[0].category.toLowerCase() + '.');
+                    $("#blacklistmessage").html(encodeURI($("input").val().toLowerCase().replace('http://','').replace('https://','').replace('www.','').split(/[/?#]/)[0]) + ' was put on the blacklist for ' + result.entries[0].category.toLowerCase() + '.');
                     strLink = '<a id="details" href="/scam/' + result.entries[0].id + '">Details <i class="chevron right small icon"></i></a>';
                 } else if(result.type == 'address') {
-					          $("#blacklistmessage").html(encodeURI($("input").val().toLowerCase()) + ' was put on the blacklist and is associated with '+ result.entries.length +' blocked domain(s)');
+					          $("#blacklistmessage").html(encodeURI($("input").val().toLowerCase().replace('http://','').replace('https://','').replace('www.','').split(/[/?#]/)[0]) + ' was put on the blacklist and is associated with '+ result.entries.length +' blocked domain(s)');
 					          strLink = '<a id="details" href="/address/' + result.input + '">Details <i class="chevron right small icon"></i></a>';
 				        } else if(result.type == 'ip') {
-					          $("#blacklistmessage").html(encodeURI($("input").val().toLowerCase()) + ' was put on the blacklist and is associated with '+ result.entries.length +' blocked domain(s)');
+					          $("#blacklistmessage").html(encodeURI($("input").val().toLowerCase().replace('http://','').replace('https://','').replace('www.','').split(/[/?#]/)[0]) + ' was put on the blacklist and is associated with '+ result.entries.length +' blocked domain(s)');
 					          strLink = '<a id="details" href="/ip/' + result.input + '">Details <i class="chevron right small icon"></i></a>';
 				        }
                 $("#blacklistmessage").html($("#blacklistmessage").html() + ' ' + strLink);
