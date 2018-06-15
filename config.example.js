6270fef1544277f733c219118b1312beb70b112e
hahnmichaelf <hahn.michael.f@gmail.com>
2018-06-15 11:36:15 -0400

    change cache refresh to 30 minutes instead of 2 hours

--- config.example.js
+++ config.example.js
@@ -1,6 +1,6 @@
 module.exports = {
     "port": 8080,
-    "cache_refreshing_interval": 1000 * 60 * 60 * 2,
+    "cache_refreshing_interval": 1000 * 60 * 30,
     "Google_SafeBrowsing_API_Key": null,
     "Github_Hook_Secret": null,
     "Urlscan_API_Key": null,
