2a845b48611726c895eca73e750aaabb2aeb029e
Michael Hahn <hahn.michael.f@gmail.com>
2018-07-10 16:37:28 -0400

    Added virustotal_api_key to config.js

--- config.example.js
+++ config.example.js
@@ -3,6 +3,7 @@ module.exports = {
     "cache_refreshing_interval": 1000 * 60 * 30,
     "Google_SafeBrowsing_API_Key": null,
     "Github_Hook_Secret": null,
+    "VirusTotal_API_Key": null,
     "Urlscan_API_Key": null,
     "AbuseIPDB_API_Key": null,
     "repository": {
