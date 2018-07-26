31286a8d25624cc5c936021db5d9170a1dd945e4
409H <harry.denley@outlook.com>
2018-07-27 00:04:10 +0100

    Issue #1184 - Added logic to not perform the dns lookup on domains

--- update.js
+++ update.js
@@ -67,112 +67,135 @@ scams.forEach(function(scam, index) {
         var scam_details = new_cache.scams[new_cache.scams.push(scam) - 1];
         new_cache.blacklist.push(url.parse(scam.url).hostname.replace("www.", ""));
         new_cache.blacklist.push('www.' + url.parse(scam.url).hostname.replace("www.", ""));
-        dns.lookup(url.parse(scam.url).hostname, (err, address, family) => {
-            if (!err) {
-                scam_details.ip = address;
+
+        // Check to see if we should hit the domain or not
+        if('perform_dns_lookup' in config && config.perform_dns_lookup === false) {
+            scam_details.status = "NotChecked";
+            scam_details.ip = "0.0.0.0";
+            console.log("Note: Not performing the DNS lookups due to configuration - "+ scam.url);
+
+            if(index == (scams.length-1)) {
+                var done_interval = setInterval(function() {
+                    if (requests_pending == 0) {
+                        clearInterval(done_interval);
+                        Object.keys(new_cache.ips).forEach(function(ip) {
+                            new_cache.blacklist.push(ip);
+                        });
+                        fs.writeFileSync("_cache/cache.json", JSON.stringify(new_cache));
+                        debug("Done");
+                        process.exit();
+                    }
+                }, 500);
             }
-            dns.resolveNs(url.parse(scam.url).hostname, (err, addresses) => {
+
+        } else {
+            dns.lookup(url.parse(scam.url).hostname, (err, address, family) => {
                 if (!err) {
-                    scam_details.nameservers = addresses;
+                    scam_details.ip = address;
                 }
-                requests_pending++;
-                var r = request(scam.url, {timeout: 5*60*1000}, function(e, response, body) {
-                    requests_pending--;
-                    if (e || !([200, 301, 302].includes(response.statusCode))) {
-                        scam_details.status = 'Offline';
-                        new_cache.inactives.push(scam);
-                    } else if (r.uri.href.indexOf('cgi-sys/suspendedpage.cgi') !== -1) {
-                        scam_details.status = 'Suspended';
-                        new_cache.inactives.push(scam);
-                    } else {
-                        if ('subcategory' in scam && scam.subcategory == 'MyEtherWallet') {
-                            requests_pending++;
-                            request('http://' + url.parse(scam.url).hostname.replace("www.", "") + '/js/etherwallet-static.min.js', {timeout: 5*60*1000}, function(e, response, body) {
-                                requests_pending--;
-                                if (!e && response.statusCode == 200) {
-                                    scam_details.status = 'Active';
-                                    new_cache.actives.push(scam);
-                                } else {
-                                    scam_details.status = 'Inactive';
-                                    new_cache.inactives.push(scam);
-                                }
-                            });
-                        } else if ('subcategory' in scam && scam.subcategory == 'MyCrypto') {
-                            requests_pending++;
-                            request('http://' + url.parse(scam.url).hostname.replace("www.", "") + '/js/mycrypto-static.min.js', {timeout: 5*60*1000}, function(e, response, body) {
-                                requests_pending--;
-                                if (!e && response.statusCode == 200) {
-                                    scam_details.status = 'Active';
-                                    new_cache.actives.push(scam);
-                                } else {
-                                    scam_details.status = 'Inactive';
-                                    new_cache.inactives.push(scam);
-                                }
-                            });
-                        } else if (body == '') {
-                            scam_details.status = 'Inactive';
+                dns.resolveNs(url.parse(scam.url).hostname, (err, addresses) => {
+                    if (!err) {
+                        scam_details.nameservers = addresses;
+                    }
+                    requests_pending++;
+                    var r = request(scam.url, {timeout: 5*60*1000}, function(e, response, body) {
+                        requests_pending--;
+                        if (e || !([200, 301, 302].includes(response.statusCode))) {
+                            scam_details.status = 'Offline';
+                            new_cache.inactives.push(scam);
+                        } else if (r.uri.href.indexOf('cgi-sys/suspendedpage.cgi') !== -1) {
+                            scam_details.status = 'Suspended';
                             new_cache.inactives.push(scam);
                         } else {
-                            scam_details.status = 'Active';
-                            new_cache.actives.push(scam);
-                        }
-                    }
-                    /*if(scam_details.status != 'Offline' && 'Urlscan_API_Key' in config) {
-                    	urlscan_timeout++;
-                    	setTimeout(function() {
-                    		request('https://urlscan.io/api/v1/scan/', { method: 'POST', json: { 'url': scam.url, 'public': 'off' }, headers: { 'API-Key': config.Urlscan_API_Key }}, function(err,response,body) {
-                    			if(err || response.statusCode != 200) {
-                    				debug(err);
-                    				debug('Status code: ' + response.statusCode);
-                    			} else if(body.message != 'Submission successful' || !('api' in body)) {
-                    				debug(body.message);
-                    			} else {
-                    				setTimeout(function() {
-                    					request(body.api, { method: 'POST', json: { 'url': scam.api, 'public': 'off' }, headers: { 'API-Key': config.Urlscan_API_Key }}, function(err,response,body) {
-                    						if(err || response.statusCode != 200) {
-                    							debug(err);
-                    							debug('Status code: ' + response.statusCode);
-                    						} else {
-                    							debug(body);
-                    						}
-                    					});
-                    				}, 2000);
-                    			}
-                    		});
-                    	}, urlscan_timeout * 8000);
-                    }*/
-                    if ('ip' in scam_details) {
-                        if (!(scam_details.ip in new_cache.ips)) {
-                            new_cache.ips[scam_details.ip] = [];
-                        }
-                        new_cache.ips[scam_details.ip] = scam_details;
-                    }
-                    if ('addresses' in scam_details) {
-                        scam_details.addresses.forEach(function(address) {
-                            if (!(address.toLowerCase() in new_cache.addresses)) {
-                                new_cache.addresses[address.toLowerCase()] = [];
+                            if ('subcategory' in scam && scam.subcategory == 'MyEtherWallet') {
+                                requests_pending++;
+                                request('http://' + url.parse(scam.url).hostname.replace("www.", "") + '/js/etherwallet-static.min.js', {timeout: 5*60*1000}, function(e, response, body) {
+                                    requests_pending--;
+                                    if (!e && response.statusCode == 200) {
+                                        scam_details.status = 'Active';
+                                        new_cache.actives.push(scam);
+                                    } else {
+                                        scam_details.status = 'Inactive';
+                                        new_cache.inactives.push(scam);
+                                    }
+                                });
+                            } else if ('subcategory' in scam && scam.subcategory == 'MyCrypto') {
+                                requests_pending++;
+                                request('http://' + url.parse(scam.url).hostname.replace("www.", "") + '/js/mycrypto-static.min.js', {timeout: 5*60*1000}, function(e, response, body) {
+                                    requests_pending--;
+                                    if (!e && response.statusCode == 200) {
+                                        scam_details.status = 'Active';
+                                        new_cache.actives.push(scam);
+                                    } else {
+                                        scam_details.status = 'Inactive';
+                                        new_cache.inactives.push(scam);
+                                    }
+                                });
+                            } else if (body == '') {
+                                scam_details.status = 'Inactive';
+                                new_cache.inactives.push(scam);
+                            } else {
+                                scam_details.status = 'Active';
+                                new_cache.actives.push(scam);
                             }
-                            //debug(new_cache.addresses);
-                            new_cache.addresses[address.toLowerCase()] = scam_details;
-                        });
-                    }
-					scams_checked++;
-					if(index == (scams.length-1)) {
-						var done_interval = setInterval(function() {
-							if (requests_pending == 0) {
-								clearInterval(done_interval);
-								Object.keys(new_cache.ips).forEach(function(ip) {
-									new_cache.blacklist.push(ip);
-								});
-								fs.writeFileSync("_cache/cache.json", JSON.stringify(new_cache));
-								debug("Done");
-								process.exit();
-							}
-						}, 500);
-					}
+                        }
+                        /*if(scam_details.status != 'Offline' && 'Urlscan_API_Key' in config) {
+                            urlscan_timeout++;
+                            setTimeout(function() {
+                                request('https://urlscan.io/api/v1/scan/', { method: 'POST', json: { 'url': scam.url, 'public': 'off' }, headers: { 'API-Key': config.Urlscan_API_Key }}, function(err,response,body) {
+                                    if(err || response.statusCode != 200) {
+                                        debug(err);
+                                        debug('Status code: ' + response.statusCode);
+                                    } else if(body.message != 'Submission successful' || !('api' in body)) {
+                                        debug(body.message);
+                                    } else {
+                                        setTimeout(function() {
+                                            request(body.api, { method: 'POST', json: { 'url': scam.api, 'public': 'off' }, headers: { 'API-Key': config.Urlscan_API_Key }}, function(err,response,body) {
+                                                if(err || response.statusCode != 200) {
+                                                    debug(err);
+                                                    debug('Status code: ' + response.statusCode);
+                                                } else {
+                                                    debug(body);
+                                                }
+                                            });
+                                        }, 2000);
+                                    }
+                                });
+                            }, urlscan_timeout * 8000);
+                        }*/
+                        if ('ip' in scam_details) {
+                            if (!(scam_details.ip in new_cache.ips)) {
+                                new_cache.ips[scam_details.ip] = [];
+                            }
+                            new_cache.ips[scam_details.ip] = scam_details;
+                        }
+                        if ('addresses' in scam_details) {
+                            scam_details.addresses.forEach(function(address) {
+                                if (!(address.toLowerCase() in new_cache.addresses)) {
+                                    new_cache.addresses[address.toLowerCase()] = [];
+                                }
+                                //debug(new_cache.addresses);
+                                new_cache.addresses[address.toLowerCase()] = scam_details;
+                            });
+                        }
+                        scams_checked++;
+                        if(index == (scams.length-1)) {
+                            var done_interval = setInterval(function() {
+                                if (requests_pending == 0) {
+                                    clearInterval(done_interval);
+                                    Object.keys(new_cache.ips).forEach(function(ip) {
+                                        new_cache.blacklist.push(ip);
+                                    });
+                                    fs.writeFileSync("_cache/cache.json", JSON.stringify(new_cache));
+                                    debug("Done");
+                                    process.exit();
+                                }
+                            }, 500);
+                        }
+                    });
                 });
             });
-        });
+        }
     } else {
         debug("Fatal error: Scam without URL found (%s)",scam.id);
         process.exit();
