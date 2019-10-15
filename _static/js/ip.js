3bc71ba0567ad53b3c19f7d6e81e4230ff204908
409H <409H@users.noreply.github.com>
2019-10-15 16:28:26 +0100

    Remove inline styling

--- _static/js/ip.js
+++ _static/js/ip.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function() {
     $.getJSON("https://freegeoip.net/json/" + $("h1").html(), function(data) {
         flag = '<i class="' + data.country_code.toLowerCase() + ' flag"></i>';
         $("#location").html('<span>' + flag + data.country_name + '</span>');
-        $("#map").html('<iframe width="100%" height="400px" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBw2MrKbJrjg6NYIq6kClIVvUx3vSj-a1s&q=' + data.latitude + ',' + data.longitude + '" allowfullscreen></iframe>');
+        $("#map").html('<iframe width="100%" height="400px" frameborder="0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBw2MrKbJrjg6NYIq6kClIVvUx3vSj-a1s&q=' + data.latitude + ',' + data.longitude + '" allowfullscreen></iframe>');
     });
 	setTimeout(function() {
 		if($("#location").html() == "loading...") {
